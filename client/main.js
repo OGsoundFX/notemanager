a04e159a6205b6ad7249b870d6796c8e8d156199
Olivier Girardot <ogmusik@gmail.com>
2020-09-22 12:45:45 +0200

    creating my very first meteor app. trying to reproduce the lexicon I did with angularjs

---
+++ client/main.js
@@ -0,0 +1,54 @@
+import { Template } from 'meteor/templating';
+
+import './main.html';
+
+Template.body.helpers({
+  lexicon:[
+    {
+      "title": "testing",
+      "topics": [],
+      "content": "",
+      "link": "",
+      "mainCategory": "Rails"
+    },
+    {
+      "title": "Trying again",
+      "topics": [],
+      "content": "",
+      "link": "",
+      "mainCategory": "AngularJS"
+    },
+    {
+      "title": "Here's a new entry",
+      "topics": [
+        "ajax",
+        "rails"
+      ],
+      "content": "This is very complicated",
+      "link": "",
+      "mainCategory": "Node.js"
+    },
+    {
+      "title": "npm",
+      "topics": [
+        "node",
+        "javascript",
+        "manager"
+      ],
+      "content": "npm is node packet manager",
+      "link": "",
+      "mainCategory": "Node.js"
+    },
+    {
+      "title": "IIFE, Immediately Invoked Function Expression:",
+      "topics": [
+        "AngularJS",
+        "javascript",
+        "function"
+      ],
+      "content": "(() => {})()\n    This is an anonymous IIFE that uses the ES6 arrow syntax. The function is () => {}, it has no names and takes no arguments.\n    It is incapsulated in () which makes it inaccessible to the general scope and has its own scope.\n    The function becomes a function expression which is immediately executed. The variable within the expression can not be accessed from outside it.\n    Assigning the IIFE to a variable stores the function's return value, not the function definition itself.",
+      "link": "",
+      "mainCategory": "Javascript"
+    }
+  ]
+});
